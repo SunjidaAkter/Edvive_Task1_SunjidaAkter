6ff2ff0f4409bb4e920dc23959fcb2ae32816e9d
Sunjida Akter <sanjidabintesayid7@gmail.com>
2022-09-18 20:57:27 +0600

    translating functions updated

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import axios from 'axios';
 
 
 function App() {
-  const [input, setInput] = useState('');
-  const [sourceCode, setSourceCode] = useState('en');
-  const [destCode, setDestCode] = useState('bn');
-  const [language, setLanguage] = useState('');
-
   const {
     transcript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
+  const [input, setInput] = useState('');
+  const [sourceCode, setSourceCode] = useState('en');
+  const [destCode, setDestCode] = useState('bn');
+  const [language, setLanguage] = useState('');
+  const [loading, setLoading] = useState(false);
+
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
@@ -53,28 +54,41 @@ function App() {
   const translate = () => {
     console.log(transcript);
     let apiURL = `https://api.mymemory.translated.net/get?q=${transcript}&langpair=${sourceCode}|${destCode}`;
+    setLoading(true);
     fetch(apiURL).then((res) => res.json()).then((data) => {
       if (data.responseData.translatedText) {
         console.log(data.responseData.translatedText);
-        setLanguage(data.responseData.translatedText)
+        setLanguage(data.responseData.translatedText);
+        setLoading(false);
       } else {
         console.log("sorry");
         setLanguage("sorry")
+        setLoading(false);
       }
     })
+
+  }
+  const start = () => {
+    setLanguage('');
+    SpeechRecognition.startListening();
+  }
+  const reset = () => {
+    setLanguage('');
+    resetTranscript();
   }
 
   return (
     <div className="App">
-      <Translator />
+      {/* <Translator /> */}
       <button onClick={translate}>Translate</button>
       <div>
         <p>Microphone: {listening ? 'on' : 'off'}</p>
-        {listening ? <button onClick={SpeechRecognition.stopListening}>Stop</button> : <button onClick={SpeechRecognition.startListening}>Start</button>}
+        {listening ? <button onClick={SpeechRecognition.stopListening}>Stop</button> : <button onClick={start}>Start</button>}
         {/* <button onClick={SpeechRecognition.stopListening}>Stop</button> */}
-        <button onClick={resetTranscript}>Reset</button>
+        <button onClick={reset}>Reset</button>
         <p>{transcript}</p>
         <p>Translated below</p>
+        {loading && <p>Loading...</p>}
         <p>{language}</p>
       </div>
     </div>
